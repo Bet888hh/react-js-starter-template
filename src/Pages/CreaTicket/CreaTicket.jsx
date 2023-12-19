a69491a84daf5f869a83103a273ab09b333beed4
mike <mgianni@pccube.com>
2023-12-19 12:02:51 +0100

    paginator

--- src/Pages/CreaTicket/CreaTicket.jsx
+++ src/Pages/CreaTicket/CreaTicket.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
-
-const CreaTicket = () => {
-  return (
-    <div>
-      Cread ticket
-    </div>
-  )
-}
 
 export default CreaTicket
+import React, { useState } from 'react';
+
+const CreaTicket = ({ ruolo, ticketAperti, operatoriSenior }) => {
+  const [titolo, setTitolo] = useState('');
+  const [testo, setTesto] = useState('');
+  const [categoria, setCategoria] = useState(''); // Categoria di default
+  const [categoriaManuale, setCategoriaManuale] = useState('');
+  const [assegnaA, setAssegnaA] = useState('');
+
+  const gestisciCreazioneTicket = () => {
+    // Verifica dei vincoli in base al ruolo dell'utente
+    if (ruolo === 'UTENTE SEMPLICE') {
+      const ticketApertiOLavorazione = ticketAperti.filter(ticket => ticket.stato === 'APERTO' || ticket.stato === 'IN LAVORAZIONE');
+      if (ticketApertiOLavorazione.length > 2) {
+        alert('Hai già aperto troppi ticket. Chiudi alcuni prima di aprirne un altro.');
+        return;
+      }
+    }
+
+    // Altri controlli e logica specifica del ticket
+
+    // Simulazione del salvataggio a DB
+    alert('Ticket creato con successo!');
+  };
+
+  return (
+    <div>
+      <h2>Creazione Ticket</h2>
+      <label>Titolo:</label>
+      <input type="text" value={titolo} onChange={e => setTitolo(e.target.value)} />
+
+      <label>Testo:</label>
+      <textarea value={testo} onChange={e => setTesto(e.target.value)} />
+
+      <label>Categoria:</label>
+      <select value={categoria} onChange={e => setCategoria(e.target.value)}>
+        {/* Opzioni categorie */}
+      </select>
+
+      {categoria === 'ALTRO' && (
+        <div>
+          <label>Categoria Manuale:</label>
+          <input type="text" value={categoriaManuale} onChange={e => setCategoriaManuale(e.target.value)} />
+        </div>
+      )}
+
+      {ruolo === 'OPERATORE JUNIOR' && (
+        <div>
+          <label>Assegna a:</label>
+          <select value={assegnaA} onChange={e => setAssegnaA(e.target.value)}>
+            {/* Opzioni operatori senior */}
+          </select>
+        </div>
+      )}
+
+      <button onClick={gestisciCreazioneTicket}>Crea Ticket</button>
+    </div>
+  );
+};
+
+export default CreaTicket;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import Paginator from '../../Components/Paginator/Paginator';
 
 const Home = () => {
+  var arrayOggetti = [
+    { campo: "valore1" },
+    { campo: "valore2" },
+    { campo: "valore3" },
+    { campo: "valore4" },
+    { campo: "valore5" },
+    { campo: "valore6" },
+];
   return (
     <div>
       Home
+
+
+
+      <table>
+        <thead>
+        <th>elem1</th>
+        </thead>
+        <tbody>
+
+        <Paginator elemPerPagina={3}>
+        {arrayOggetti.map((elem, index) => (
+            <tr key={index}>{elem.campo}</tr>
+          ))}
+        </Paginator>
+        </tbody>
+      </table>
     </div>
   )
 }
