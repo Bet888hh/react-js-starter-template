d53cb115dc913ca6d8520956da2d2391678812ed
Alberto Salone <asalone@pccube.com>
2023-12-20 12:00:55 +0100

    adsfasdfasdfsad

--- src/Pages/MieiTicket/MieiTicket.jsx
+++ src/Pages/MieiTicket/MieiTicket.jsx
@@ -1,9 +1,15 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import PulsantieraTable from '../../Components/PulsantieraTable/PulsantieraTable'
-import Paginator from '../../Components/Paginator/Paginator';
-import SortableTableHead from '../../Components/SortableTable/SortableTableHead';
-import PulsantieraFiltri from '../../Components/PulsantieraFiltri/PulsantieraFiltri';
-import { headers, urlbase } from '../../Utility/urls';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import PulsantieraTable from "../../Components/PulsantieraTable/PulsantieraTable";
+import Paginator from "../../Components/Paginator/Paginator";
+import SortableTableHead from "../../Components/SortableTable/SortableTableHead";
+import PulsantieraFiltri from "../../Components/PulsantieraFiltri/PulsantieraFiltri";
+import { headers, urlbase } from "../../Utility/urls";
 import { useSelector } from "react-redux";
 
 import { SelectUserSlice } from "../../store/Reducer/Slices/UserSlice/UserSlice";
@@ -13,41 +19,77 @@ const MieiTicket = () => {
   const [filter, setFilter] = useState("");
   const user = useSelector(SelectUserSlice);
   //cose da mettere in un hook personalizzato
+  const init = useCallback(async ()=> {
+    const stati = ["APERTO", "IN_LAVORAZIONE", "CHIUSO"];
+    const responses = await Promise.all(
+      stati.map((stato) =>
+        fetch(
+          urlbase("TICKET") +
+            `?queries[0]=search("Stato", ["${stato}"])&queries[1]=search("Utente", ["${user.Username}"])`,
+          {
+            method: "GET",
+            headers: headers,
+          }
+        )
+      )
+    );
+    const jsonResponses = await Promise.all(
+      responses.map((response) => response.json())
+    );
+    const [
+      { documents: dAperti, total: totalAperti },
+      { documents: dlavorazione, total: totalLavorazione },
+      { documents: dchiusi, total: totalChiusi },
+    ] = jsonResponses;
 
-  const deletePost = useCallback(async (id)=>{
-    const response = await fetch(
-      urlbase("TICKET") + "/"+id,
-      {
+    setTotali((prev) => ({
+      ...prev,
+      aperti: totalAperti,
+      chiusi: totalChiusi,
+      inLavorazione: totalLavorazione,
+    }));
+  },[])
+
+
+
+  const deletePost = useCallback(
+    async (id) => {
+      const response = await fetch(urlbase("TICKET") + "/" + id, {
         method: "delete",
         headers: headers,
+      });
+      if (response.status === 204) {
+        console.log("olè");
+        setElementi((prev) => prev.filter((el) => el.$id !== id));
+        init();
       }
-    );
-    if(response.status===204){
-      console.log("olè");
-      setElementi(prev=> prev.filter(el=>el.$id!==id))
-      init()
-    }
-  },[])
-  const handleTableAction = (e) => {
-    console.log(e);
-    const [action,id]=e.split("-");
-    switch (action) {
-      case("rimuovi"):
-      deletePost(id);
-      break;
-    }
-  };
-  
+    },
+    [init]
+  );
 
-  const [totali,setTotali]= useState({aperti:-1,chiusi:-1,inLavorazione:-1, inCarico:-1})
-  const onSort = (campo) => {
-    const nuovoOrdine =
-      sortConfig.current.campo === campo && sortConfig.current.ordine === "asc"
-        ? "desc"
-        : "asc";
-    sortConfig.current = { campo: campo, ordine: nuovoOrdine };
-    sortElementi();
-  };
+
+  const handleTableAction = useCallback(
+    (e) => {
+      console.log(e);
+      const [action, id] = e.split("-");
+      switch (action) {
+        case "rimuovi":
+          deletePost(id);
+          break;
+      }
+    },
+    [deletePost]
+  );
+
+  const [totali, setTotali] = useState({
+    aperti: -1,
+    chiusi: -1,
+    inLavorazione: -1,
+    inCarico: -1,
+  });
+
+
+
   const sortElementi = useCallback(() => {
     const datiClone = [...elementi];
 
@@ -86,50 +128,67 @@ const MieiTicket = () => {
     setElementi(datiClone);
   }, [elementi, sortConfig]);
 
-  const handleFiltra = async (e) => {
-    let data = null;
-    if (filter === e) {
-      setElementi([]);
-      setFilter("");
-    } else {
-      switch (e) {
-        case "APERTO":
-          setFilter(e);
-          data = await takeData(e);
-          break;
-        case "IN_LAVORAZIONE":
-          setFilter(e);
-          data = await takeData(e);
-          break;
-        case "CHIUSI":
-          setFilter(e);
-          data = await takeData(e);
-          break;
-      }
-      setElementi(data);
-    }
-  };
+  const onSort = useCallback((campo) => {
+    const nuovoOrdine =
+      sortConfig.current.campo === campo && sortConfig.current.ordine === "asc"
+        ? "desc"
+        : "asc";
+    sortConfig.current = { campo: campo, ordine: nuovoOrdine };
+    sortElementi();
+  },[sortElementi]);
 
-  const takeData = async (type) => {
-    const response = await fetch(
-      urlbase("TICKET") + `?queries[0]=search("Stato",+["${type}"])&queries[1]=search("Utente",+["${user.Username}"])`,
-      {
-        method: "GET",
-        headers: headers,
-      }
-    );
-    const rs = await response.json();
 
-    if (rs.message) {
-      //errori cazzo
-    } else {
-      return rs.documents.map((doc) => ({
-        ...doc,
-        ApertoIl: doc.$createdAt,
-        UltimaModifica: doc.$updatedAt,
-      }));
-    }
-  };
+
+  const takeData = useCallback(
+    async (type) => {
+      const response = await fetch(
+        urlbase("TICKET") +
+          `?queries[0]=search("Stato",+["${type}"])&queries[1]=search("Utente",+["${user.Username}"])`,
+        {
+          method: "GET",
+          headers: headers,
+        }
+      );
+      const rs = await response.json();
+
+      if (rs.message) {
+        //errori cazzo
+      } else {
+        return rs.documents.map((doc) => ({
+          ...doc,
+          ApertoIl: doc.$createdAt,
+          UltimaModifica: doc.$updatedAt,
+        }));
+      }
+    },
+    [user.Username]
+  );
+  const handleFiltra = useCallback(
+    async (e) => {
+      let data = null;
+      if (filter === e) {
+        setElementi([]);
+        setFilter("");
+      } else {
+        switch (e) {
+          case "APERTO":
+            setFilter(e);
+            data = await takeData(e);
+            break;
+          case "IN_LAVORAZIONE":
+            setFilter(e);
+            data = await takeData(e);
+            break;
+          case "CHIUSI":
+            setFilter(e);
+            data = await takeData(e);
+            break;
+        }
+        setElementi(data);
+      }
+    },
+    [filter, takeData]
+  );
 
   const perTabella = useMemo(() => {
     return elementi
@@ -142,7 +201,7 @@ const MieiTicket = () => {
             UltimaModifica: e.UltimaModifica,
             Operatore: e.operatore,
             Messaggi: e.Messaggi,
-         
+
             Azioni: (
               <PulsantieraTable
                 id={e.$id}
@@ -153,15 +212,15 @@ const MieiTicket = () => {
           };
         })
       : null;
-  }, [elementi]);
+  }, [elementi, handleTableAction]);
 
-  const tableBody = useMemo(() => {});
+  
 
   const intestazioni = perTabella.length > 0 ? Object.keys(perTabella[0]) : [];
   const excludeFromSorting = ["Azioni"];
   const includeInTableIf = { filter: "nan", include: "nan" };
 
-  const formatCell = (intestazione, valore, riga) => {
+  const formatCell = useCallback((intestazione, valore, riga) => {
     switch (intestazione) {
       case "Categoria":
         return valore === "ALTRO"
@@ -175,34 +234,14 @@ const MieiTicket = () => {
       default:
         return valore || "-";
     }
-  };
-  async function init (){
-   const stati = ["APERTO", "IN_LAVORAZIONE", "CHIUSO"];
-  const responses = await Promise.all(
-    stati.map((stato) =>
-      fetch(
-        urlbase("TICKET") + `?queries[0]=search("Stato", ["${stato}"])&queries[1]=search("Utente", ["${user.Username}"])`,
-        {
-          method: "GET",
-          headers: headers,
-        }
-      )
-    )
-    
-    
-  );
-  const jsonResponses = await Promise.all(
-    responses.map((response) => response.json())
-  );
-  const [{documents:dAperti,total:totalAperti},{documents:dlavorazione,total:totalLavorazione},{documents:dchiusi,total:totalChiusi}]=jsonResponses;
-  
-  setTotali(prev=>({...prev,aperti:totalAperti,chiusi:totalChiusi,inLavorazione:totalLavorazione}))
-  }
-  useEffect(()=>{
-  
-  init()
-  
-  },[])
+  },[]);
+ 
+
+  useEffect(() => {
+    init();
+  }, []);
+
+
   return (
     <div>
       <PulsantieraFiltri totali={totali} handleFiltra={handleFiltra} />
@@ -238,6 +277,6 @@ const MieiTicket = () => {
       )}
     </div>
   );
-}
+};
 
-export default MieiTicket
+export default MieiTicket;
