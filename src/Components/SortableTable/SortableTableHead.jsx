fb9d9c71e9ff83dedc154cf279bb88ab099873a2
Alberto Salone <asalone@pccube.com>
2023-12-20 10:13:25 +0100

    jkhfkjhgjkgkjhkjhgkjhgkjhg

--- src/Components/SortableTable/SortableTableHead.jsx
+++ src/Components/SortableTable/SortableTableHead.jsx
@@ -1,25 +1,35 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
 
-const SortableTableHead = ({intestazioni,onSort,excludeFromSorting}) => {
+const SortableTableHead = ({intestazioni,onSort,excludeFromSorting,includeInTableIf,filter}) => {
         const handleSort = (e) => {
           onSort(e.target.id);
         };
   return (
     <>
- <thead>
-      <tr>
-        {intestazioni.map((intestazione) => (
-          <th key={intestazione}>
-            {!excludeFromSorting.includes(intestazione)?<button id={intestazione} onClick={handleSort}>
-              {intestazione} &#x2195;
-            </button>:<span>{intestazione}</span>}
-          </th>
-        ))}
-      </tr>
-    </thead>
+      <thead>
+        <tr>
+          {intestazioni.map((intestazione) => (
+            <>
+              {((intestazione === includeInTableIf.include &&
+                includeInTableIf.filter === filter) ||
+                intestazione !== includeInTableIf.include) && (
+                <th key={intestazione}>
+                  {!excludeFromSorting.includes(intestazione) ? (
+                    <button id={intestazione} onClick={handleSort}>
+                      {intestazione} &#x2195;
+                    </button>
+                  ) : (
+                    <span>{intestazione}</span>
+                  )}
+                </th>
+              )}
+            </>
+          ))}
+        </tr>
+      </thead>
     </>
-  )
+  );
 }
 
 export default SortableTableHead
--- src/Pages/GestioneTicket/GestioneTicket.jsx
+++ src/Pages/GestioneTicket/GestioneTicket.jsx
@@ -3,11 +3,11 @@ import PulsantieraTable from "../../Components/PulsantieraTable/PulsantieraTable
 import PulsantieraFiltri from "../../Components/PulsantieraFiltri/PulsantieraFiltri";
 import { headers, urlbase } from "../../Utility/urls";
 import Paginator from "../../Components/Paginator/Paginator";
-import SortableTableHead from "../../Components/SortableTable/SortableTableHead"
+import SortableTableHead from "../../Components/SortableTable/SortableTableHead";
 import SortableTableRows from "../../Components/SortableTable/SortableTableRows";
 const GestioneTicket = () => {
   const [elementi, setElementi] = useState([]);
-  const sortConfig= useRef({ campo: "Titolo", ordine: "asc" });
+  const sortConfig = useRef({ campo: "Titolo", ordine: "asc" });
   const [filter, setFilter] = useState("");
 
   //cose da mettere in un hook personalizzato
@@ -20,22 +20,25 @@ const GestioneTicket = () => {
       sortConfig.current.campo === campo && sortConfig.current.ordine === "asc"
         ? "desc"
         : "asc";
-  sortConfig.current = {campo : campo, ordine :nuovoOrdine}
+    sortConfig.current = { campo: campo, ordine: nuovoOrdine };
     sortElementi();
   };
   const sortElementi = useCallback(() => {
     const datiClone = [...elementi];
-  
+
     if (sortConfig.current.campo) {
       datiClone.sort((a, b) => {
         const valoreA = a[sortConfig.current.campo];
         const valoreB = b[sortConfig.current.campo];
-  
+
         // Parsing delle date se i campi sono "ApertoIl" o "UltimaModifica"
-        if (sortConfig.current.campo === 'ApertoIl' || sortConfig.current.campo === 'UltimaModifica') {
+        if (
+          sortConfig.current.campo === "ApertoIl" ||
+          sortConfig.current.campo === "UltimaModifica"
+        ) {
           const dataA = new Date(valoreA);
           const dataB = new Date(valoreB);
-  
+
           if (dataA < dataB) {
             return sortConfig.current.ordine === "asc" ? -1 : 1;
           }
@@ -44,7 +47,7 @@ const GestioneTicket = () => {
           }
           return 0;
         }
-  
+
         // Gestione generale per gli altri tipi di dati
         if (valoreA < valoreB) {
           return sortConfig.current.ordine === "asc" ? -1 : 1;
@@ -57,14 +60,12 @@ const GestioneTicket = () => {
     }
     setElementi(datiClone);
   }, [elementi, sortConfig]);
-  
-
 
   const handleFiltra = async (e) => {
     let data = null;
     if (filter === e) {
       setElementi([]);
-      setFilter("")
+      setFilter("");
     } else {
       switch (e) {
         case "APERTO":
@@ -100,9 +101,11 @@ const GestioneTicket = () => {
     if (rs.message) {
       //errori cazzo
     } else {
- 
-      return rs.documents.map(doc=>({...doc,ApertoIl:doc.$createdAt,UltimaModifica:doc.$updatedAt}))
-      
+      return rs.documents.map((doc) => ({
+        ...doc,
+        ApertoIl: doc.$createdAt,
+        UltimaModifica: doc.$updatedAt,
+      }));
     }
   };
 
@@ -117,6 +120,7 @@ const GestioneTicket = () => {
             UltimaModifica: e.UltimaModifica,
             Operatore: e.operatore,
             Messaggi: e.Messaggi,
+            Assegnatario: "",
             Azioni: (
               <PulsantieraTable
                 id={e.$id}
@@ -128,48 +132,59 @@ const GestioneTicket = () => {
         })
       : null;
   }, [elementi]);
-  
-const tableBody = useMemo(()=>{
-  
-})
 
+  const tableBody = useMemo(() => {});
 
-  
-  const intestazioni =perTabella.length>0? Object.keys(perTabella[0]):[]
-  const excludeFromSorting = ["Azioni"]
+  const intestazioni = perTabella.length > 0 ? Object.keys(perTabella[0]) : [];
+  const excludeFromSorting = ["Azioni"];
+  const includeInTableIf = { filter: "in-carico", include: "Assegnatario" };
 
-
-const formatCell = (intestazione, valore, riga) => {
-  switch (intestazione) {
-    case 'Categoria':
-      return valore === 'ALTRO' ? `${valore} - ${riga.categoria_manuale || '-'}` : valore;
-    case 'ApertoIl':
-    case 'UltimaModifica':
-      return valore ? new Date(valore).toLocaleString('it-IT') : '-';
-    case 'Messaggi':
-      return"dafa"
-    default:
-      return valore || '-';
-  }
-};
+  const formatCell = (intestazione, valore, riga) => {
+    switch (intestazione) {
+      case "Categoria":
+        return valore === "ALTRO"
+          ? `${valore} - ${riga.categoria_manuale || "-"}`
+          : valore;
+      case "ApertoIl":
+      case "UltimaModifica":
+        return valore ? new Date(valore).toLocaleString("it-IT") : "-";
+      case "Messaggi":
+        return "dafa";
+      default:
+        return valore || "-";
+    }
+  };
   return (
     <div>
       <PulsantieraFiltri handleFiltra={handleFiltra} />
-      {(elementi.length>0 && intestazioni.length > 0) && (
+      {elementi.length > 0 && intestazioni.length > 0 && (
         <>
-          <SortableTableHead excludeFromSorting={excludeFromSorting} intestazioni={intestazioni} onSort={onSort}/>
-        <tbody>
-
-         <Paginator elemPerPagina={5}>
-         {perTabella.map((riga, index) => (
-           <tr key={index}>
-        {intestazioni.map((intestazione) => (
-          <td key={intestazione}>{formatCell(intestazione, riga[intestazione])}</td>
-          ))}
-      </tr>
-    ))}
-          </Paginator> 
-    </tbody>
+          <SortableTableHead
+            filter={filter}
+            includeInTableIf={includeInTableIf}
+            excludeFromSorting={excludeFromSorting}
+            intestazioni={intestazioni}
+            onSort={onSort}
+          />
+          <tbody>
+            <Paginator elemPerPagina={5}>
+              {perTabella.map((riga, index) => (
+                <tr key={index}>
+                  {intestazioni.map((intestazione) => (
+                    <>
+                      {((intestazione === includeInTableIf.include &&
+                        includeInTableIf.filter === filter) ||
+                        intestazione !== includeInTableIf.include) && (
+                        <td key={intestazione}>
+                          {formatCell(intestazione, riga[intestazione])}
+                        </td>
+                      )}
+                    </>
+                  ))}
+                </tr>
+              ))}
+            </Paginator>
+          </tbody>
         </>
       )}
     </div>
