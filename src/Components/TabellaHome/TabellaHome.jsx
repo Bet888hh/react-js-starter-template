8102a95066fa0798cd2415935f2445b314bd4ff1
mike <mgianni@pccube.com>
2023-12-21 11:08:04 +0100

    Crea ticket modifica patch

--- src/Components/TabellaHome/TabellaHome.jsx
+++ src/Components/TabellaHome/TabellaHome.jsx
@@ -2,7 +2,35 @@ import React from 'react'
 
 const TabellaHome = () => {
   return (
-    <div>TabellaHome</div>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Titolo</th>
+            <th>Testo</th>
+            <th>Azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Titolo 1</td>
+            <td>Testo 1</td>
+            <td>
+              <button>Modifica</button>
+              <button>Elimina</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Titolo 2</td>
+            <td>Testo 2</td>
+            <td >
+              <button>Modifica</button>
+              <button>Elimina</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
